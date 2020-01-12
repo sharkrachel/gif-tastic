e6f3c57a79adefc49ed4279953f9f915e6aab108
Rachel Sipes <rasipes@gmail.com>
2020-01-12 11:25:05 -0500

    added in psuedocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+// research gif api
+// create api query url variable
+// create a variable 'topics' that includes an array of strings
+// create buttons for all topics (probably use a for loop)
+// when user clicks button, display 10 static, non-animated gif images
+// when user clicks gif image, the image animates
+// when user clicks gif image again, the image is static
+// under each gif, display the gif rating
+// when user clicks 'submit' under form, create a new button with the value of the form input
+// consider changing value to lowercase to keep consistent
+// rejoice in making an api website all by yourself
