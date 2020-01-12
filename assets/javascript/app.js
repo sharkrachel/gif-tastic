5f68d5563339998c257e6dba0ae94102aeb69812
Rachel Sipes <rasipes@gmail.com>
2020-01-12 18:49:51 -0500

    added js to populate array into buttons, display gifs and animate on click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,3 +9,59 @@
 // when user clicks 'submit' under form, create a new button with the value of the form input
 // consider changing value to lowercase to keep consistent
 // rejoice in making an api website all by yourself
+
+// array of topics 
+var topic = ["dog", "kitten", "fish", "elephant", "hedgehog", "pig"]
+
+// giphy api url
+// var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=FiWNirzIJ7fzTeTf89RrLK6Tmp4aosFw"
+
+// create a button for each item in topic array
+function createButton() {
+    for (var i = 0; i < topic.length; i++) {
+        var button = $("<button>");
+        button.attr("class", "giphyBtn btn btn-warning ml-2");
+        button.attr("data-topic", topic[i]);
+        button.text(topic[i]);
+        $(".buttons").append(button);
+    }
+}
+
+$(document).on("click", ".giphyBtn", function() {
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + $(this).attr("data-topic") + "&api_key=FiWNirzIJ7fzTeTf89RrLK6Tmp4aosFw&limit=10"
+    $(".gifs").empty();
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+        for (var i = 0; i < response.data.length; i++) {
+            var img = $("<img>");
+            img.attr("class", "animateImage")
+            img.attr("src",response.data[i].images.original_still.url)
+            img.attr("data-animate", response.data[i].images.original.url);
+            img.attr("data-still",response.data[i].images.original_still.url);
+            img.attr("data-status", "still");
+            img.attr("data-status", "animate");
+            $(".gifs").prepend(img);
+
+        }
+        $(".animateImage").on("click", function(){
+            var status = $(this).attr("data-status");
+            if (status === "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-status", "animate");
+            }
+
+            else {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-status", "still");
+            }
+
+        })
+
+    })
+} )
+
+createButton();
+
